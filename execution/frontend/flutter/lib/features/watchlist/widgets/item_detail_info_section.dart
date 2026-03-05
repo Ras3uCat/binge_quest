03992378181d8a5713724b35ec5d95fec483e81c
rmr32 <rmr@gmx.us>
2026-03-04 18:40:24 -0600

    crashlytics fixes

--- execution/frontend/flutter/lib/features/watchlist/widgets/item_detail_info_section.dart
+++ execution/frontend/flutter/lib/features/watchlist/widgets/item_detail_info_section.dart
@@ -51,6 +51,12 @@ class ItemDetailInfoSection extends StatelessWidget {
                       width: 100,
                       height: 150,
                       fit: BoxFit.cover,
+                      errorWidget: (_, __, ___) => Container(
+                        width: 100,
+                        height: 150,
+                        color: EColors.surfaceLight,
+                        child: const Icon(Icons.movie, size: 40),
+                      ),
                     )
                   : Container(
                       width: 100,
